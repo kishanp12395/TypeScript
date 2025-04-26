086bfaf34ee84f12ec5618a628634d52d09e51e9
kishanp12395 <kkp12395@gmail.com>
2025-04-26 10:10:13 +0530

    refactor: update bigint example and add explanatory comments

--- src/6bigint/1bigint.ts
+++ src/6bigint/1bigint.ts
@@ -1,4 +1,17 @@
 console.log('BigInt');
-console.log(1n + 2n); // 3n
-console.log('jii');
-console.log('kojd');
+var number: bigint= 1234567890123456789012345678901234567890n;
+var x = 1n;
+var y = 4n;
+
+console.log(number + x); // 1234567890123456789012345678901234567893
+console.log(number + y); // 1234567890123456789012345678901234567893
+
+// by adding in larg number it will not change the value of number
+
+
+
+// when bigint is added to number it will convert the number to bigint
+
+// bigint will added to only bigint and number will added to only number
+
+
