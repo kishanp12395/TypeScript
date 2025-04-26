e6396c80804e80694e589c038006a250cf7979bd
kishanp12395 <kkp12395@gmail.com>
2025-04-26 10:18:32 +0530

    refactor: remove old bigint files and add updated implementation

---
+++ output/6bigintnum/1bigint.js
@@ -0,0 +1,10 @@
+"use strict";
+console.log('BigInt');
+var number = 1234567890123456789012345678901234567890n;
+var x = 1n;
+var y = 4n;
+console.log(number + x); // 1234567890123456789012345678901234567893
+console.log(number + y); // 1234567890123456789012345678901234567893
+// by adding in larg number it will not change the value of number
+// when bigint is added to number it will convert the number to bigint
+// bigint will added to only bigint and number will added to only number
